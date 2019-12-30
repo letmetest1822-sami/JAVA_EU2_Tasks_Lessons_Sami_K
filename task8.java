334ef4e070150872b1c5bde12ad5d4db7d4e1d81
Sami <59291934+letmetest1822-sami@users.noreply.github.com>
2019-12-30 16:39:07 +0300

    Add files via upload

---
+++ task8.java
@@ -0,0 +1,17 @@
+package day3_variables_dataTypes;
+
+public class task8 {
+
+	public static void main(String[] args) {
+		String sentence1 = "IN 1491 COLOMBUS SAILED THE OCEANBLUE";
+				System.out.println(sentence1  + 	"\n");
+				
+		String word1 = "IN ";
+		int  year2 = 1492;
+		String Word2 = " COLOMBUS SAILED THE OCEANBLUE";
+		System.out.println(word1 + year2 + Word2);
+				
+
+	}
+
+}
